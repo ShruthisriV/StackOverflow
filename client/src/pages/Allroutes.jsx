009a4c67ed1696de348bc07f85903aa53c13a147
ShruthisriV <shruthiraoveerla06@gmail.com>
2025-03-15 20:00:58 +0530

    Ask question page

--- client/src/pages/Allroutes.jsx
+++ client/src/pages/Allroutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
+import Askquestion from './Askquestion/Askquestion';
 
 function Allroutes({slidein, handleslidein}) {
   return (
     <>
         <Routes>
-            <Route path='/' element={<Home slidein={slidein} handleslidein={handleslidein}/>}>
-            </Route>
+            <Route path='/' element={<Home slidein={slidein} handleslidein={handleslidein}/>}/>
+            <Route path='/Askquestion' element={<Askquestion />}/>
         </Routes>
     </>
   )
