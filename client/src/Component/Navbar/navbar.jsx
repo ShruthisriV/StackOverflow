548855a2ebac4d8679c13a79f6f6cc4fa1811d50
ShruthisriV <shruthiraoveerla06@gmail.com>
2025-03-16 21:57:22 +0530

    Auth page (signup and login) is designed

--- client/src/Component/Navbar/navbar.jsx
+++ client/src/Component/Navbar/navbar.jsx
@@ -1,55 +1,72 @@
-import React from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import {useSelector, useDispatch} from 'react-redux';
-import bars from '../../assets/bars-solid.svg';
-import logo from '../../assets/logo.png';
-import search from '../../assets/search-solid.svg';
-import Avatar from '../Avatar/Avatar';
-import './navbar.css';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import bars from "../../assets/bars-solid.svg";
+import logo from "../../assets/logo.png";
+import search from "../../assets/search-solid.svg";
+import Avatar from "../Avatar/Avatar";
+import "./navbar.css";
 
-function navbar({handleslidein}) {
-    var User = null;
+function Navbar({ handleslidein }) {
+  var User = null;
+  const navigate = useNavigate();
   return (
     <>
       <nav className="main-nav">
         <div className="navbar">
-            <button className="slide-in-icon" onClick={() => handleslidein()}>
-                <img src={bars} alt="bars" width='15'/>
-            </button>
-            <div className="navbar-1">
-                <Link to='/' className='nav-item nav-logo'>
-                    <img src={logo} alt="logo"/>
-                </Link>
-                <Link to='/' className='nav-item nav-btn res-nav'>
-                    About
-                </Link>
-                <Link to='/' className='nav-item nav-btn res-nav'>
-                    Products
-                </Link>
-                <Link to='/' className='nav-item nav-btn res-nav'>
-                    For Teams
-                </Link>
-                <form><input type="text" placeholder='Search...'/>
-                <img src={search} alt="search" width='18' className='search-icon' /></form>
-            </div>
-            <div className="navbar-2">
-                {User===null ? (
-                    <Link to='/' className='nav-item nav-links'>
-                        Log in
-                    </Link>
-                ):(
-                    <>
-                        <Avatar backgroundColor='#009dff' px='10px' py='7px' borderRadius='50%' color="white">
-                            <Link to='/' style={{color: "white", textDecoration:"none"}}></Link>
-                        </Avatar>
-                        <button className="nav-item nav-links ">Log out</button>
-                    </>
-                )}
-            </div>
+          <button className="slide-in-icon" onClick={() => handleslidein()}>
+            <img src={bars} alt="bars" width="15" />
+          </button>
+          <div className="navbar-1">
+            <Link to="/" className="nav-item nav-logo">
+              <img src={logo} alt="logo" />
+            </Link>
+            <Link to="/" className="nav-item nav-btn res-nav">
+              About
+            </Link>
+            <Link to="/" className="nav-item nav-btn res-nav">
+              Products
+            </Link>
+            <Link to="/" className="nav-item nav-btn res-nav">
+              For Teams
+            </Link>
+            <form>
+              <input type="text" placeholder="Search..." />
+              <img
+                src={search}
+                alt="search"
+                width="18"
+                className="search-icon"
+              />
+            </form>
+          </div>
+          <div className="navbar-2">
+            {User === null ? (
+              <Link to="/Auth" className="nav-item nav-links">
+                Log in
+              </Link>
+            ) : (
+              <>
+                <Avatar
+                  backgroundColor="#009dff"
+                  px="10px"
+                  py="7px"
+                  borderRadius="50%"
+                  color="white"
+                >
+                  <Link
+                    to="/"
+                    style={{ color: "white", textDecoration: "none" }}
+                  ></Link>
+                </Avatar>
+                <button className="nav-item nav-links ">Log out</button>
+              </>
+            )}
+          </div>
         </div>
       </nav>
     </>
   );
 }
 
-export default navbar;
+export default Navbar;
--- client/src/pages/Allroutes.jsx
+++ client/src/pages/Allroutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import Askquestion from './Askquestion/Askquestion';
+import Auth from './Auth/Auth';
 
 function Allroutes({slidein, handleslidein}) {
   return (
@@ -9,6 +10,7 @@ function Allroutes({slidein, handleslidein}) {
         <Routes>
             <Route path='/' element={<Home slidein={slidein} handleslidein={handleslidein}/>}/>
             <Route path='/Askquestion' element={<Askquestion />}/>
+            <Route path='/Auth' element={<Auth />}/>
         </Routes>
     </>
   )
